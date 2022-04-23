5d43ae31d0e7c5052dcef194e832341ff22e0f2a
tunnex92 <tunnex92@gmail.com>
2022-04-23 09:04:17 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,31 @@
+// var num1 = parseInt(prompt("Input First Number"));
+// var num2 = parseInt(prompt("Input Second Number"));
+// sum = num1 + num2
+// output1="The Sum of " +num1 + "&" + num2 + "="+ sum
+// alert(output1)
+
+// Assignment 2
+
+// var firstname = prompt("Input Your First Name");
+// var Lastname = prompt("Input Your Last Name");
+// var fourchar = firstname.slice(0,4);
+// var lastchar = Lastname.slice(0,1);
+// var combine = fourchar + lastchar;
+
+// var userid = combine.toLowerCase();
+
+// var output2 = "Your CapacityBay userId is " + userid;
+// alert(output2)
+
+Task
+
+var username = prompt("Enter your name");
+var user = username.slice(0,1);
+var fupper = user.toUpperCase();
+var trial = username.slice(1);
+var lower = trial.toLowerCase();
+var final = fupper + lower;
+// var capital = username.toUpperCase();
+var output = "Welcome back, " + final + " you are good to go";
+alert(output);
+
